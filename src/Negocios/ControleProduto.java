a1f694f537c2eb82cf4e494ef66b39ef486ce592
alan.marinho <alan.marinho@N000001.parvi.local>
2019-05-28 18:00:32 -0300

    Atualização Controle Produto

--- src/Negocios/ControleProduto.java
+++ src/Negocios/ControleProduto.java
@@ -11,11 +11,16 @@ public class ControleProduto {
 	//fazer a regra de negocio pra produtos depois
 	
 	public void cadastrar(Produtos produto) throws InserirException {
-		if ((produto).getChassi().length() != 17 || (produto).getChassi().equals("")) {
-			InserirException e;
-			e = new InserirException((produto).getChassi());
-			throw e;
+		if (produto.getDescricao().equals("") || produto.getDescricao() == null) {
+			InserirException e1;
+			e1 = new InserirException(produto.getDescricao());
+			throw e1;
         }
+		if (produtos.procurar(produto.getDescricao()).getCodigo().equals(produto.getDescricao())){
+			InserirException e2;
+			e2 = new InserirException(produto.getDescricao());
+			throw e2;
+		}
 		
 					produtos.inserir(produto);
 					
@@ -31,12 +36,12 @@ public class ControleProduto {
 					produtos.alterar(produto);
 		}
      
-	public Produtos procurar(String chassi) throws NaoLocalizadoProdutoException{
-		if (produtos.procurar(chassi).getCodigo() == null) {
-			throw new NaoLocalizadoProdutoException(chassi);
+	public Produtos procurar(String codigo) throws NaoLocalizadoProdutoException{
+		if (produtos.procurar(codigo).getCodigo() == null) {
+			throw new NaoLocalizadoProdutoException(codigo);
 		}else {
 			
-		return produtos.procurar(chassi);
+		return produtos.procurar(codigo);
 		
 		}
 	}
